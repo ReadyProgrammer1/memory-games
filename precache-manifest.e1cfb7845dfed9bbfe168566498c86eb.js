6a0f2ceadee5d6b5aceb32aa8920fc1d2b3af9e0
Daniel Payne <42071158+ReadyProgrammer1@users.noreply.github.com>
2018-11-09 14:16:38 -0500

    Add files via upload

---
+++ precache-manifest.e1cfb7845dfed9bbfe168566498c86eb.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d2f59d0ea3d1eb1b8806808e18a47b1",
+    "url": "/memory-games/static/media/painted.3d2f59d0.gif"
+  },
+  {
+    "revision": "24008a98d8703b82410e",
+    "url": "/memory-games/static/css/main.a146e2e7.chunk.css"
+  },
+  {
+    "revision": "8ec0840322b1e9fe97e5",
+    "url": "/memory-games/static/js/1.8ec08403.chunk.js"
+  },
+  {
+    "revision": "ad64fb68f8606d1992ac",
+    "url": "/memory-games/static/js/runtime~main.ad64fb68.js"
+  },
+  {
+    "revision": "7df6e67f834c347beb55a857f526e59b",
+    "url": "/memory-games/static/media/yacht.7df6e67f.png"
+  },
+  {
+    "revision": "f9fa784583b98a8eceffeb371c4f80dd",
+    "url": "/memory-games/static/media/castle.f9fa7845.png"
+  },
+  {
+    "revision": "eb321eb36d72c9a065615e231150eb55",
+    "url": "/memory-games/static/media/volcano.eb321eb3.png"
+  },
+  {
+    "revision": "fdf2a113814a6c1b824465e9e9c56aa0",
+    "url": "/memory-games/static/media/forest.fdf2a113.png"
+  },
+  {
+    "revision": "a9d235e371c3296a5ff1fc1eaabb08e8",
+    "url": "/memory-games/static/media/paradise.a9d235e3.png"
+  },
+  {
+    "revision": "777a452dd1403b2d072829e3b6b13464",
+    "url": "/memory-games/static/media/dragons.777a452d.png"
+  },
+  {
+    "revision": "4638f0a02866c33c97e81d26d940d2cb",
+    "url": "/memory-games/static/media/football.4638f0a0.png"
+  },
+  {
+    "revision": "02665d5305546e1d18081a45eed25359",
+    "url": "/memory-games/static/media/purplespaceship.02665d53.png"
+  },
+  {
+    "revision": "f2312ba2c34913acaf7dc54a8f7bcb12",
+    "url": "/memory-games/static/media/zoo.f2312ba2.png"
+  },
+  {
+    "revision": "5338baa606f85281f125593cde982764",
+    "url": "/memory-games/static/media/guerilla.5338baa6.gif"
+  },
+  {
+    "revision": "c3500ecc5bc0cb2586325be5d07e2bb7",
+    "url": "/memory-games/static/media/elephant.c3500ecc.gif"
+  },
+  {
+    "revision": "7a86511be083c089020dd6c32c411cf3",
+    "url": "/memory-games/static/media/giraffe.7a86511b.gif"
+  },
+  {
+    "revision": "66194bc217320c4d2ce263a84c975b40",
+    "url": "/memory-games/static/media/lions.66194bc2.gif"
+  },
+  {
+    "revision": "c23231f1c09e37d45409ed418fc829db",
+    "url": "/memory-games/static/media/rino.c23231f1.gif"
+  },
+  {
+    "revision": "a3b2954213010c21deff6b95c6224ef1",
+    "url": "/memory-games/static/media/toucan.a3b29542.gif"
+  },
+  {
+    "revision": "c0906ae3c387cf485fc0b1f919ce2228",
+    "url": "/memory-games/static/media/Panda.c0906ae3.gif"
+  },
+  {
+    "revision": "07a4a89637c8ce2ad4049f64d7c5b1e5",
+    "url": "/memory-games/static/media/pandafall.07a4a896.gif"
+  },
+  {
+    "revision": "384677c8daea7f767478f7dce4e2ede4",
+    "url": "/memory-games/static/media/castle.384677c8.gif"
+  },
+  {
+    "revision": "5eeb8ccd30f29888296b52bf035753b1",
+    "url": "/memory-games/static/media/crown.5eeb8ccd.gif"
+  },
+  {
+    "revision": "c5cb9543cfb2978cd579178f35326303",
+    "url": "/memory-games/static/media/dance.c5cb9543.gif"
+  },
+  {
+    "revision": "e60b4793d98b844a041438a26b1623ef",
+    "url": "/memory-games/static/media/flowers.e60b4793.gif"
+  },
+  {
+    "revision": "2ea93f859266f42578e219cc618bed7a",
+    "url": "/memory-games/static/media/horse.2ea93f85.gif"
+  },
+  {
+    "revision": "5b6027179a9fca7f363f2ef7c0f5b70b",
+    "url": "/memory-games/static/media/knight.5b602717.gif"
+  },
+  {
+    "revision": "b4bf011c205a04c5229ee2b28f85a5b9",
+    "url": "/memory-games/static/media/queen.b4bf011c.gif"
+  },
+  {
+    "revision": "ca09693f5e1cd01f06f799a4be5078dc",
+    "url": "/memory-games/static/media/sparkles.ca09693f.gif"
+  },
+  {
+    "revision": "3014eac5fe50c90f2ed6ab3241900f58",
+    "url": "/memory-games/static/media/bird.3014eac5.gif"
+  },
+  {
+    "revision": "3b1367a4a6da1ef601be0c6533007782",
+    "url": "/memory-games/static/media/colibri.3b1367a4.gif"
+  },
+  {
+    "revision": "cad6b67bcd84b48fec2ce1abb1dbe1c6",
+    "url": "/memory-games/static/media/dove.cad6b67b.gif"
+  },
+  {
+    "revision": "8814e0931212cdfd1cf1b15ab268c4cf",
+    "url": "/memory-games/static/media/fast.8814e093.gif"
+  },
+  {
+    "revision": "f2f616b59d4874a1ca2b1ba274e40061",
+    "url": "/memory-games/static/media/flying.f2f616b5.gif"
+  },
+  {
+    "revision": "76b16190c366093c43b6a9b92e912002",
+    "url": "/memory-games/static/media/lightgreen.76b16190.gif"
+  },
+  {
+    "revision": "24008a98d8703b82410e",
+    "url": "/memory-games/static/js/main.24008a98.chunk.js"
+  },
+  {
+    "revision": "549a510def47d503a054861aa638db4b",
+    "url": "/memory-games/static/media/teal.549a510d.gif"
+  },
+  {
+    "revision": "4703f6812fdf1f819e23ee35ac84fa8e",
+    "url": "/memory-games/static/media/battle.4703f681.gif"
+  },
+  {
+    "revision": "e8db26796f4d9855e6cf0f4a13a2b00a",
+    "url": "/memory-games/static/media/falcon.e8db2679.gif"
+  },
+  {
+    "revision": "8465c46d829356cd512ebf66d296cab5",
+    "url": "/memory-games/static/media/galaxy.8465c46d.gif"
+  },
+  {
+    "revision": "647e0804677a61b11e62cda43f448f68",
+    "url": "/memory-games/static/media/nebula.647e0804.gif"
+  },
+  {
+    "revision": "813108f7fa86d0e62529b21f0a9fabea",
+    "url": "/memory-games/static/media/rotate.813108f7.gif"
+  },
+  {
+    "revision": "8eadfb03bcda57ed389956e1e6bcae77",
+    "url": "/memory-games/static/media/spin.8eadfb03.gif"
+  },
+  {
+    "revision": "d6432dd6009656b086b13ddc54e004e8",
+    "url": "/memory-games/static/media/star.d6432dd6.gif"
+  },
+  {
+    "revision": "30a92d9c00b3fed65b4be6edfe9779f6",
+    "url": "/memory-games/static/media/ties.30a92d9c.gif"
+  },
+  {
+    "revision": "e97f65c71073fdca706a9940b98a2782",
+    "url": "/memory-games/static/media/boulders.e97f65c7.gif"
+  },
+  {
+    "revision": "ddb12616c619f672588d06b3602795a7",
+    "url": "/memory-games/static/media/closeup.ddb12616.gif"
+  },
+  {
+    "revision": "cc330fca0bbfe0a6cfcbfc6d1b3cac90",
+    "url": "/memory-games/static/media/deer.cc330fca.gif"
+  },
+  {
+    "revision": "950054f4196e82b84dc04cd592a3f810",
+    "url": "/memory-games/static/media/fall.950054f4.gif"
+  },
+  {
+    "revision": "9633cf0626f175325ee2c6e1a42ac0ef",
+    "url": "/memory-games/static/media/falls.9633cf06.gif"
+  },
+  {
+    "revision": "e3a12a03c11be439cb5fe112f765c31f",
+    "url": "/memory-games/static/media/flower.e3a12a03.gif"
+  },
+  {
+    "revision": "63e6bb82cbdb3e836e2477ef07db7fab",
+    "url": "/memory-games/static/media/green.63e6bb82.gif"
+  },
+  {
+    "revision": "b8a5140b9cb3980321868766a59a281f",
+    "url": "/memory-games/static/media/stream.b8a5140b.gif"
+  },
+  {
+    "revision": "b895b68cea0d5897e9798f2524ad5e05",
+    "url": "/memory-games/static/media/blue.b895b68c.gif"
+  },
+  {
+    "revision": "022a3be4781fef1e4afd73085024cdd8",
+    "url": "/memory-games/static/media/boat.022a3be4.gif"
+  },
+  {
+    "revision": "8ad0b99c998266e1f2ecbb8f62c97466",
+    "url": "/memory-games/static/media/curl.8ad0b99c.gif"
+  },
+  {
+    "revision": "5cb6cb3efc973869a72181d44870c713",
+    "url": "/memory-games/static/media/flash.5cb6cb3e.gif"
+  },
+  {
+    "revision": "46fce7fe157aeaaa7bc7680628b21d22",
+    "url": "/memory-games/static/media/mountain.46fce7fe.gif"
+  },
+  {
+    "revision": "4d1e755cfeb7839d27dd97b255029cc3",
+    "url": "/memory-games/static/media/sunset.4d1e755c.gif"
+  },
+  {
+    "revision": "b48f92187e78c6371149d6f39e6b13b5",
+    "url": "/memory-games/static/media/surf.b48f9218.gif"
+  },
+  {
+    "revision": "37716a05761d5f94a6819c06d06c0d48",
+    "url": "/memory-games/static/media/tree.37716a05.gif"
+  },
+  {
+    "revision": "aabe9b2009711a4b0b58f06d24b556e9",
+    "url": "/memory-games/static/media/bird.aabe9b20.gif"
+  },
+  {
+    "revision": "199e48daa0f1b7f6ced263316f761960",
+    "url": "/memory-games/static/media/green.199e48da.gif"
+  },
+  {
+    "revision": "cb33a6fe014be202dd8700ed6fc9feeb",
+    "url": "/memory-games/static/media/orange.cb33a6fe.gif"
+  },
+  {
+    "revision": "1d217ddd8b6b5a4fbf49ab744be855af",
+    "url": "/memory-games/static/media/purple.1d217ddd.gif"
+  },
+  {
+    "revision": "5d27099922e5fa89a861563885703ace",
+    "url": "/memory-games/static/media/red.5d270999.gif"
+  },
+  {
+    "revision": "5fa7c7dd3c56998b5c944ef18a55bce6",
+    "url": "/memory-games/static/media/rider.5fa7c7dd.gif"
+  },
+  {
+    "revision": "c24c9822c16d5916dd2fe715d378904b",
+    "url": "/memory-games/static/media/swim.c24c9822.gif"
+  },
+  {
+    "revision": "ce6a6ee2e788579bf8ced9b2c7daceb9",
+    "url": "/memory-games/static/media/toothless.ce6a6ee2.gif"
+  },
+  {
+    "revision": "3aec5e25d9b25c82998929478b613245",
+    "url": "/memory-games/index.html"
+  }
+];
